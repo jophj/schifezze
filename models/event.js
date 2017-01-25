815ab9acfbe4cd867e35d6fe2ee042fc68c48d46
Giovanni Pinto <giovanni.pinto.po@gmail.com>
2017-01-25 15:31:52 +0100

    Events are saved

--- models/event.js
+++ models/event.js
@@ -2,10 +2,11 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var eventSchema = new Schema({
+  user: String,  
   type: String,
+  value: Number,
   date: Date,
-  description: String,
-  value: Number
+  description: String
 });
 
 var Event = mongoose.model('Event', eventSchema);
