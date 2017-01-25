d54c6254883e086fa716f2518646a1c23bb90ae6
Giovanni Pinto <giovanni.pinto.po@gmail.com>
2017-01-25 11:17:56 +0100

    Sample response built

--- bot-modules/schifezze-bot.js
+++ bot-modules/schifezze-bot.js
@@ -10,8 +10,24 @@ class SchifezzaBotModule extends BotModule {
       const query = inlineQuery.query || '';
       const command = queryParser.parse(query);
       if (command) {
-        
+        const results = [
+          {
+            id: `${Math.random() * 2048}`,
+            title: 'Result title ' + Math.random() * 2048,
+            type: 'article',
+            input_message_content: {
+              message_text: `stocazzo`,
+              parse_mode: 'Markdown'
+            }
+          }
+        ]
+
+        return ctx.answerInlineQuery(results);
       }
+    });
+
+    this.bot.on('chosen_inline_result', (ctx) => {
+      console.log(ctx);
     });
   }
 }
